46783371c96ee6fd1843f216e8cee41a55b0be55
raveenadayani <raveenadayani@gmail.com>
2017-07-27 03:15:50 +0530

    chng in footer

--- src/FooterContent.js
+++ src/FooterContent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { blueGrey800, fullBlack } from 'material-ui/styles/colors';
-const style = { display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '..', height: '..' }
-const style2 = { paddingTop: 5, display: 'flex', justifyContent: 'center', alignItems: 'center', width: '..', height: '..' }
+const style = { display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '..', height: '..', paddingRight:5 }
+const style2 = { paddingBottom:5,paddingTop: 5, display: 'flex', justifyContent: 'center', alignItems: 'center', width: '..', height: '..' }
 
 
 export default class FooterContent extends React.Component {
@@ -11,19 +11,19 @@ export default class FooterContent extends React.Component {
                 <div style={style}>
                     <img src='/imgs/Microsoft_footer_logo.png' />
                     <span style={{ color: blueGrey800 }}>©2017 Microsoft</span>
-                    <img src='/imgs/TheGarage_logo_small.png' />
+                    <img src='/imgs/TheGarage_logo_small.png' style={{paddingRight:5}}/>
                 </div>
                 <div style={style2}>
-                    <span style={{ color: blueGrey800 }}> Microsoft Confidential</span>
-                    <span style={{ color: fullBlack }} >&nbsp;|&nbsp;</span>
-
                     <span style={{ color: blueGrey800 }}><a style={{ color: blueGrey800, textDecoration: 'none' }} target="_blank" href='https://privacy.microsoft.com/en-gb/privacystatement' rel="noopener noreferrer" >Privacy</a> </span>
                     <span style={{ color: fullBlack }}>&nbsp;|&nbsp;</span>
 
                     <span style={{ color: blueGrey800 }}>                    <a style={{ color: blueGrey800, textDecoration: 'none' }} target="_blank" href='https://www.microsoft.com/EN-US/privacystatement/DPN/Default.aspx' rel="noopener noreferrer" >Protection</a></span>
                     <span style={{ color: fullBlack }}>&nbsp;|&nbsp;</span>
 
-                    <span style={{ color: blueGrey800 }}>                    <a style={{ color: blueGrey800, textDecoration: 'none' }} target="_blank" href='https://garagehackbox.azurewebsites.net/msft/policies' rel="noopener noreferrer" >Policies</a> </span>
+                    <span style={{ color: blueGrey800 }}><a style={{ color: blueGrey800, textDecoration: 'none' }} target="_blank" href='https://garagehackbox.azurewebsites.net/msft/policies' rel="noopener noreferrer" >Policies</a> </span>
+                    <span style={{ color: fullBlack }}>&nbsp;|&nbsp;</span>
+
+                    <span style={{ color: blueGrey800 }}><a style={{ color: blueGrey800, textDecoration: 'none' }} target="_blank" href='mailto:sfapphelp@microsoft.com' rel="noopener noreferrer" >Queries?</a> </span>
                 </div>
             </div>
         );
